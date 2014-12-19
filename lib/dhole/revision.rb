b192d5c0040b746df28004f0c05c74ed7452fa9a
Asaf Bartov <asaf.bartov@gmail.com>
2014-12-18 20:40:24 -0800

    PK for revision

--- lib/dhole/revision.rb
+++ lib/dhole/revision.rb
@@ -1,6 +1,7 @@
 module Dhole
   class Revision < ActiveRecord::Base
     self.table_name = 'revision'
+    self.primary_key = 'rev_id'
     belongs_to :text, :class_name => 'Text', :foreign_key => 'rev_text_id'
   end
 end
