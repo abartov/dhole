6a1d7ad5411639e36ceeb7376c5282073d2f093a
Asaf Bartov <asaf.bartov@gmail.com>
2014-12-20 09:49:41 -0800

    Implemented basic Category and CategoryLink
    So far, just a Category#members method, returning an array of
    CategoryLinks, and a Category#member_pages convenience method returning
    an array of Pages only.

--- lib/dhole/category.rb
+++ lib/dhole/category.rb
@@ -3,5 +3,16 @@ module Dhole
     self.table_name = 'category'
     self.primary_key = 'cat_id'
     # cat_files, cat_title
+    
+    def members
+      CategoryLink.where(cl_to: cat_title) || []
+    end
+    def member_pages
+      members.select{|cl| cl.cl_type == 'page'}.map {|cl| Page.find(cl.cl_from)}
+    end
+    # return only members of specified type ('file','page', 'subcat')
+    def members_by_type(mtype)
+      # TBD
+    end
   end
 end
--- lib/dhole/categorylink.rb
+++ lib/dhole/categorylink.rb
@@ -1,6 +1,6 @@
 module Dhole
   class CategoryLink < ActiveRecord::Base
-    self.table_name = 'categorylink'
+    self.table_name = 'categorylinks'
     belongs_to :page, class_name: 'Page', foreign_key: 'cl_from'
     # cat_files, cat_title
   end
