2c2486dcc147125a01d52d610cf37e3da9d47a99
Asaf Bartov <asaf.bartov@gmail.com>
2014-12-26 13:19:14 -0800

    corrected new category methods

--- lib/dhole/category.rb
+++ lib/dhole/category.rb
@@ -8,10 +8,10 @@ module Dhole
       CategoryLink.where(cl_to: cat_title) || []
     end
     def member_page_titles
-      members.select{|cl| cl.cl_type == 'page'}.map {|cl| cl.cl_from}
+      members.select{|cl| cl.cl_type == 'page'}.map {|cl| Page.find(cl.cl_from).page_title}
     end
     def member_file_titles
-      members.select{|cl| cl.cl_type == 'file'}.map {|cl| cl.cl_from}
+      members.select{|cl| cl.cl_type == 'file'}.map {|cl| Image.find(cl.cl_from).img_name}
     end
     def member_pages
       members.select{|cl| cl.cl_type == 'page'}.map {|cl| Page.find(cl.cl_from)}
